695e69c2c0899fd7026fb6eb3eab44ac3c792dce
梁俊可 <2583493258@qq.cpm>
2022-05-20 12:36:03 +0800

    20220520_day01入门

--- Day01_初识Python/04_格式化输出.py
+++ Day01_初识Python/04_格式化输出.py
@@ -7,3 +7,7 @@ print('我的名字是:%s' % name)
 print('我的年龄为:%d岁' % age)
 print("我的体重为:%.2f公斤" % weight)
 print("我的学号是:%03d" % stu_id)  # 以0补全
+
+print('%s的年龄为:%d岁,体重为:%.2f公斤,学号是:%06d' % (name, age, weight, stu_id))
+
+print('%s明年的年龄为:%d岁' % (name, age+1))
