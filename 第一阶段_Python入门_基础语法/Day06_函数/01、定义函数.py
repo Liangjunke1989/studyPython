694d4fbe422f62d15e7c5f141d07669f3651e6bc
liangjunke <liangjunke198909@outlook.com>
2025-08-23 09:51:57 +0800

    20250823_day06

--- 第一阶段_Python入门_基础语法/Day06_函数/01、定义函数.py
+++ 第一阶段_Python入门_基础语法/Day06_函数/01、定义函数.py
@@ -8,7 +8,6 @@ def display01(a):
     """说明文档位置"""
     return print('显示"我是新定义的函数"')
 
-
 print("__________________________________")
 
 
@@ -16,7 +15,7 @@ def display02(b):
     """
     函数说明文档展示
     :param b 字符串参数
-    :return 打印输出参数:
+    :return 打印输出参数
     """
     return print('我是函数定义案例展示！')
 
