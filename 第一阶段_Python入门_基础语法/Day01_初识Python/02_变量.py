26fbc1893ede62699746faaf35d5cf19fc0205fe
liangjunke <liangjunke198909@outlook.com>
2025-09-17 10:31:56 +0800

    20250921_day06_01

--- 第一阶段_Python入门_基础语法/Day01_初识Python/02_变量.py
+++ 第一阶段_Python入门_基础语法/Day01_初识Python/02_变量.py
@@ -1,6 +1,20 @@
 # 定义变量
+
+"""
+    变量的命名规则：
+        1、变量的名称只能由 字母、数字、下划线组成.
+        2、变量不能以数字开头
+        3、严格区分大小写
+        4、不能使用Python关键字
+"""
+
 my_name = 'ljk'
 print(my_name)
 
 schoolName = '华鸿慧中'
 print(schoolName)
+
+_=123
+print(_)
+
+help('keywords')
